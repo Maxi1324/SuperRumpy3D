49c6b18ce0863ecbdab7d32ab3a01dc46d31bfcb
marko4499 <mark10rabe@gmail.com>
2022-01-15 21:11:42 +0100

    höhlenlevel
    höhlenlevelscene

---
+++ COOLESGAMEProjekt/Assets/Game/Scripts/Generell/LevelManagersd/LevelManager/HöhlenLevel/HöhlenLevelManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Generell.LevelManager1{
+    public class HöhlenLevelManager : LevelManager
+    {
+        public override void ResetLevel()
+        {
+        }
+    }
+}
